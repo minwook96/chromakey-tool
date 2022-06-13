009a65c0a2d4fad72cc07e471698116344cbf0d8
Minwook Hwang <alsdnr325@gmail.com>
2022-06-13 16:20:45 +0900

    first commit

--- banner.py
+++
@@ -1,3 +0,0 @@
-from art import text2art
-a = text2art("skysys")
-print(a)
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ BANNER = """
         ██████╔╝██║░╚██╗░░░██║░░░██████╔╝░░░██║░░░██████╔╝
         ╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═════╝░░░░╚═╝░░░╚═════╝░
           
-          		        [italic]Chromakey TOOL (skysys) V0.1[/italic]
+          		        [italic]Chroma key TOOL (skysys) V0.1[/italic]
 
 =====================================================================[/bold blue]                                                                                                                                
 """
@@ -35,7 +35,7 @@ def main():
     """
     pass
 
-def create_chromakey(images_path, background_path, type):
+def create_chromakey(images_path, background_path, location_type):
     # img 파일만 가져오기
     images_list = os.listdir(images_path)
     file_list_img = [file for file in images_list if file.endswith(".jpg")]
@@ -51,7 +51,7 @@ def create_chromakey(images_path, background_path, type):
             # --② ROI 선택을 위한 좌표 계산
             height1, width1 = img1.shape[:2]
             height2, width2 = img2.shape[:2]
-            if type == 1:
+            if location_type == 1:
                 x = width2 - width1
                 y = height2 - height1
                 # 랜덤 위치
@@ -85,13 +85,13 @@ def create_chromakey(images_path, background_path, type):
             # --⑥ 마스크 생성 및 마스킹 후 합성
             mask = cv2.inRange(hsv_img, lower, upper)
             mask_inv = cv2.bitwise_not(mask)
-            if type == 1:
+            if location_type == 1:
                 roi = img2[rand_y:rand_h, rand_x:rand_w]  # 랜덤 위치
             else:
                 roi = img2[y:h, x:w]  # 고정 위치
             fg = cv2.bitwise_and(img1, img1, mask=mask_inv)
             bg = cv2.bitwise_and(roi, roi, mask=mask)
-            if type == 1:
+            if location_type == 1:
                 img2[rand_y:rand_h, rand_x:rand_w] = fg + bg  # 랜덤 위치
             else:
                 img2[y:h, x:w] = fg + bg  # 고정 위치
@@ -118,20 +118,17 @@ def version():
     """
     Shows what version idt is currently on
     """
-    click.clear()
-    console.print("[bold magenta]Chromakey Tool (skysys)[/bold magenta] version 0.1")
+    console.print("[bold]Chroma key Tool (skysys)[/bold] version 0.1")
 
 @main.command()
 def authors():
     """
     Shows who are the creators of IDT
     """
-    click.clear()
-    console.print(
-        "[bold]Chromakey Tool (skysys)[/bold] email [bold magenta]skysys@skysys.co.kr[/bold magenta] tel [bold red]052) 707-7561 [/bold red]")
+    console.print("[bold]Chroma key Tool (skysys)[/bold] email [bold magenta]skysys@skysys.co.kr[/bold magenta] tel [bold red]052) 707-7561 [/bold red]")
 
 @main.command()
-@click.option('--default', '-d', '--d', is_flag=True, default=False, help="Generate a default config file")
+@click.option('--default', '-d', is_flag=True, default=False, help="Generate a default config file")
 def init(default):
     """
     This command initialyzes idt and creates a dataset config file
@@ -141,87 +138,89 @@ def init(default):
 
     if default:
         document_dict = {
-            "images_path": "images",
-            "background_path": "background",
-            "dataset": 50,
+            "images_path": "fg",
+            "background_path": "bg",
+            "type": 1,
             "IMAGE_SIZE": 512,
-            "RESIZE_METHOD": "longer_side",
-            "CLASSES": [{"CLASS_NAME": "Test", "SEARCH_KEYWORDS": "images of cats"}]}
+            "CLASSES": [{"CLASS_NAME": "Test", "SEARCH_KEYWORDS": "images of cats"}]
+        }
+        create_chromakey(document_dict["images_path"], document_dict["background_path"], document_dict["type"])
 
-    while True:
-        images_path = click.prompt("Insert your Chroma key images path")
-        try:
-            file_list = os.listdir(images_path)
-            file_list_img = [file for file in file_list if file.endswith(".jpg")]
-            if not file_list_img:
-                raise
-        except FileNotFoundError:
-            console.print("[bold red]Error: Invalid images file path.[/bold red]")
-        except:
-            console.print("[bold red]Error: chromakey images file does not exist.[/bold red]")
-        else:
-            break
+    else:
+        while True:
+            images_path = click.prompt("Insert your Chroma key images path")
+            try:
+                file_list = os.listdir(images_path)
+                file_list_img = [file for file in file_list if file.endswith(".jpg")]
+                if not file_list_img:
+                    raise
+            except FileNotFoundError:
+                console.print("[bold red]Error: Invalid images file path.[/bold red]")
+            except:
+                console.print("[bold red]Error: chromakey images file does not exist.[/bold red]")
+            else:
+                break
 
-    console.clear()
-    console.print(BANNER)
-    while True:
-        background_path = click.prompt("Insert your background images path")
-        try:
-            file_list = os.listdir(background_path)
-            file_list_img = [file for file in file_list if file.endswith(".jpg")]
-            if not file_list_img:
-                raise
-        except FileNotFoundError:
-            console.print("[bold red]Error: Invalid background images path.[/bold red]")
-        except:
-            console.print("[bold red]Error: background images file does not exist.[/bold red]")
-        else:
-            break
+        console.clear()
+        console.print(BANNER)
+        while True:
+            background_path = click.prompt("Insert your background images path")
+            try:
+                file_list = os.listdir(background_path)
+                file_list_img = [file for file in file_list if file.endswith(".jpg")]
+                if not file_list_img:
+                    raise
+            except FileNotFoundError:
+                console.print("[bold red]Error: Invalid background images path.[/bold red]")
+            except:
+                console.print("[bold red]Error: background images file does not exist.[/bold red]")
+            else:
+                break
 
-    console.clear()
-    console.print(BANNER)
-    console.print("[bold]Choose image position[/bold]", justify="left")
-    console.print("""
-    [1] random [bold blue](recommended)[/bold blue]
-    [2] center
-    """)
-    type = click.prompt("What image location do you want?", type=int)
+        console.clear()
+        console.print(BANNER)
+        console.print("[bold]Choose image position[/bold]", justify="left")
+        console.print("""
+        [1] random [bold blue](recommended)[/bold blue]
+        [2] center
+        """)
+        location_type = click.prompt("What image location do you want?", type=int)
 
-    # console.clear()
-    # console.print(BANNER)
-    # dataset = click.prompt("Number of datasets to create per Chroma Key image: ", type=int)
+        # console.clear()
+        # console.print(BANNER)
+        # dataset = click.prompt("Number of datasets to create per Chroma Key image: ", type=int)
 
-    #     console.clear()
-    #     console.print(BANNER)
-    #     console.print("[bold]Choose image resolution[/bold]", justify="left")
-    #     console.print("""
-    # [1] 512 pixels / 512 pixels [bold blue](recommended)[/bold blue]
-    # [2] 1024 pixels / 1024 pixels
-    # [3] 256 pixels / 256 pixels
-    # [4] 128 pixels / 128 pixels
-    # [5] Keep original image size
-    #
-    # [italic]ps: note that the aspect ratio of the image will [bold]not[/bold] be changed, so possibly the images received will have slightly different size[/italic]
-    # """)
-    #
-    #     image_size_ratio = click.prompt("What is the desired image size ratio", type=int)
-    #     while image_size_ratio < 1 or image_size_ratio > 5:
-    #         console.print("[italic red]Invalid option, please choose between 1 and 5. [/italic red]")
-    #         image_size_ratio = click.prompt("\nOption: ", type=int)
-    #
-    #     if image_size_ratio == 1:
-    #         image_size_ratio = 512
-    #     elif image_size_ratio == 2:
-    #         image_size_ratio = 1024
-    #     elif image_size_ratio == 3:
-    #         image_size_ratio = 256
-    #     elif image_size_ratio == 4:
-    #         image_size_ratio = 128
-    #     elif image_size_ratio == 5:
-    #         image_size_ratio = 0
+        #     console.clear()
+        #     console.print(BANNER)
+        #     console.print("[bold]Choose image resolution[/bold]", justify="left")
+        #     console.print("""
+        # [1] 512 pixels / 512 pixels [bold blue](recommended)[/bold blue]
+        # [2] 1024 pixels / 1024 pixels
+        # [3] 256 pixels / 256 pixels
+        # [4] 128 pixels / 128 pixels
+        # [5] Keep original image size
+        #
+        # [italic]ps: note that the aspect ratio of the image will [bold]not[/bold] be changed, so possibly the images received will have slightly different size[/italic]
+        # """)
+        #
+        #     image_size_ratio = click.prompt("What is the desired image size ratio", type=int)
+        #     while image_size_ratio < 1 or image_size_ratio > 5:
+        #         console.print("[italic red]Invalid option, please choose between 1 and 5. [/italic red]")
+        #         image_size_ratio = click.prompt("\nOption: ", type=int)
+        #
+        #     if image_size_ratio == 1:
+        #         image_size_ratio = 512
+        #     elif image_size_ratio == 2:
+        #         image_size_ratio = 1024
+        #     elif image_size_ratio == 3:
+        #         image_size_ratio = 256
+        #     elif image_size_ratio == 4:
+        #         image_size_ratio = 128
+        #     elif image_size_ratio == 5:
+        #         image_size_ratio = 0
 
-    # console.clear()
-    create_chromakey(images_path, background_path, type)
+        # console.clear()
+        create_chromakey(images_path, background_path, location_type)
 
 
 if __name__ == "__main__":
